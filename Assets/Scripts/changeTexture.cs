5a68edad1cbbcba7dbf9cad5330ad72fc5b1db54
YuanMiao <1936387797@qq.com>
2020-09-24 11:38:16 +0800

    vr视频 9.24

---
+++ Assets/Scripts/changeTexture.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeTexture : MonoBehaviour
+{
+    public Material[] material; 
+    private void Update()
+    {
+        float time = 0; 
+       
+            for (int i = 0; i < material.Length; )
+            {
+                time += Time.deltaTime;
+            if (time > 0.6)
+            {
+                GameObject go = GameObject.Find("moviexlz");
+                go.GetComponent<MeshRenderer>().material = material[i];
+                time = 0;
+                i++;
+            }  
+        } 
+    }
+}
